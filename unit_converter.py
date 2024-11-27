90b8c4f97b43ecc1a98c1587ee18e6b65dfd5b7e
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2024-11-26 20:13:15 -0500

    first commit

---
+++ unit_converter.py
@@ -0,0 +1,41 @@
+# need to do as a command  line 
+# user inputs the amount thy want to convert
+#the user choose the convert from and to
+# have the user pick the type either length: millimeter, centimeter, meter, kilometer, inch, foot, yard, mile.
+# weight:milligram, gram, kilogram, ounce, pound , temperature:  Celsius, Fahrenheit, Kelvin
+# lets do temperature first
+
+# convert c to f
+
+# now how do we get the user to pick what they want to convert and how much they want to convert it too.
+# lets use user input
+# have it so that the user wants to convert Celsius to Fahrenheit
+print("please chose what temperature would you like to convert to. \n 1.  Celsius to Fahrenheit \n 2.  Fahrenheit to Celsius")
+userConvert = input()
+print("You picked ", userConvert)
+print("Please input the amount you want to convert")
+
+userAmountTemp = input()
+print("You want to convert ", userAmountTemp)
+c = 12
+amount = c * 1.8
+convertCToF = amount + 32
+print("Converted Celsius to ",c,"Fahrenheit:",round(convertCToF,2))
+
+#convert f to c
+
+f = 60
+math = f - 32
+convertFToC = math / 1.8
+print("Celsius:",round(convertFToC,2))
+
+# convert f to k
+subSum = f - 32
+multiblySum = subSum * 5
+dividSum = multiblySum / 9
+finalKelvinTotal = dividSum + 273.15
+print("Fahrenheit to Kelvin:",round(finalKelvinTotal,2))
+
+# convert c to k
+finalCelsiusToKelvin = c + 273.15
+print("Celsius to Kelvin:", round(finalCelsiusToKelvin,2))
