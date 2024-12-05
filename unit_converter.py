55e232d8476cf445c1749b209057b7e596790474
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2024-12-04 20:22:28 -0500

    added user choice

--- unit_converter.py
+++ unit_converter.py
@@ -1,4 +1,5 @@
 import sys
+from enum import Enum
 # need to do as a command  line then do
 # user inputs the amount thy want to convert
 #the user choose the convert from and to
@@ -11,7 +12,9 @@ import sys
 # now how do we get the user to pick what they want to convert and how much they want to convert it too.
 # lets use user input
 # have it so that the user wants to convert Celsius to Fahrenheit
-print("please chose what temperature would you like to convert to. \n 1.  Celsius to Fahrenheit \n 2.  Fahrenheit to Celsius")
+# CREATE pairs
+
+print("Please chose what temperature would you like to convert to. \n 1.Celsius to Fahrenheit \n 2.Fahrenheit to Celsius \n 3.Fahrenheit to Kelvin \n 4.Celsius to Kelvin")
 userConvert = input()
 print("You picked ", userConvert)
 print("Please input the amount you want to convert")
@@ -25,19 +28,28 @@ if(int(userConvert) == 1):
     print("Converted Celsius to ",c,"Fahrenheit:",round(convertCToF,2))
     sys.exit("stop program")
 #convert f to c
+if(int(userConvert) == 2):
+    print("You want to convert ", userAmountTemp)
+    f = int(userAmountTemp)
+    math = f - 32
+    convertFToC = math / 1.8
+    print("Celsius:",round(convertFToC,2))
+    sys.exit("stop program")
 
-f = 60
-math = f - 32
-convertFToC = math / 1.8
-print("Celsius:",round(convertFToC,2))
-
+if(int(userConvert) == 3):
 # convert f to k
-subSum = f - 32
-multiblySum = subSum * 5
-dividSum = multiblySum / 9
-finalKelvinTotal = dividSum + 273.15
-print("Fahrenheit to Kelvin:",round(finalKelvinTotal,2))
+    print("You want to convert ", userAmountTemp)
+    f = int(userAmountTemp)
+    subSum = f - 32
+    multiblySum = subSum * 5
+    dividSum = multiblySum / 9
+    finalKelvinTotal = dividSum + 273.15
+    print("Fahrenheit to Kelvin:",round(finalKelvinTotal,2))
+    sys.exit("stop program")
 
+if(int(userConvert) == 4):
 # convert c to k
-finalCelsiusToKelvin = c + 273.15
-print("Celsius to Kelvin:", round(finalCelsiusToKelvin,2))
+    print("You want to convert ", userAmountTemp)
+    c = int(userAmountTemp)
+    finalCelsiusToKelvin = c + 273.15
+    print("Celsius to Kelvin:", round(finalCelsiusToKelvin,2))
