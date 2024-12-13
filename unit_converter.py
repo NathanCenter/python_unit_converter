c66890d657dcadaa5cb7495f091bf3d883026d73
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2024-12-12 21:38:04 -0500

    shows user flow

--- unit_converter.py
+++ unit_converter.py
@@ -12,20 +12,25 @@ from enum import Enum
 # now how do we get the user to pick what they want to convert and how much they want to convert it too.
 # lets use user input
 # have it so that the user wants to convert Celsius to Fahrenheit
-# CREATE pairs
+# CREATE pairs this may not work the way I want it to go
+class userPick(Enum):
+    Celsius_to_Fahrenheit = 1
+    Fahrenheit_to_Celsius = 2
+    Fahrenheit_to_Kelvin = 3
+    Celsius_to_Kelvin = 4
 
 print("Please chose what temperature would you like to convert to. \n 1.Celsius to Fahrenheit \n 2.Fahrenheit to Celsius \n 3.Fahrenheit to Kelvin \n 4.Celsius to Kelvin")
 userConvert = input()
-print("You picked ", userConvert)
+userChoice = int(userConvert)
+print("You picked ", str(userPick(userChoice)).replace('userPick.',''))
 print("Please input the amount you want to convert")
 
 userAmountTemp = input()
 if(int(userConvert) == 1):
-    print("You want to convert ", userAmountTemp)
     c = int(userAmountTemp)
     amount = c * 1.8
     convertCToF = amount + 32
-    print("Converted Celsius to ",c,"Fahrenheit:",round(convertCToF,2))
+    print("Converted Celsius",c, "to","Fahrenheit:",round(convertCToF,2))
     sys.exit("stop program")
 #convert f to c
 if(int(userConvert) == 2):
@@ -33,24 +38,22 @@ if(int(userConvert) == 2):
     f = int(userAmountTemp)
     math = f - 32
     convertFToC = math / 1.8
-    print("Celsius:",round(convertFToC,2))
+    print("Converted Fahrenheit",f, "to","Celsius:",round(convertFToC,2))
     sys.exit("stop program")
 
 if(int(userConvert) == 3):
 # convert f to k
-    print("You want to convert ", userAmountTemp)
     f = int(userAmountTemp)
     subSum = f - 32
     multiblySum = subSum * 5
     dividSum = multiblySum / 9
     finalKelvinTotal = dividSum + 273.15
-    print("Fahrenheit to Kelvin:",round(finalKelvinTotal,2))
+    print("Converted Fahrenheit",f, "to","Kelvin:",round(finalKelvinTotal,2))
     sys.exit("stop program")
 
 if(int(userConvert) == 4):
 # convert c to k
-    print("You want to convert ", userAmountTemp)
     c = int(userAmountTemp)
     finalCelsiusToKelvin = c + 273.15
-    print("Celsius to Kelvin:", round(finalCelsiusToKelvin,2))
+    print("Converted Celsius",c, "to","Kelvin:",round(finalCelsiusToKelvin,2))
     sys.exit("stop program")
