fecae2591ab0e23f22c70aece0be5bddecf58d06
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2025-02-08 19:26:40 -0500

    added another webpage

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/page2')
+def page2():
+    return render_template("page2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
