fa40d640cc96b0b7a72717f72d1bba076fa3ca75
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2025-02-10 20:31:52 -0500

    added page three

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ def index():
 @app.route('/page2')
 def page2():
     return render_template("page2.html")
+@app.route('/page3')
+def page3():
+    return render_template("page3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
